128f94822a1476006674cbd6deca05efb1ace260
eugene choi <eugene.maestro.choi@gmail.com>
2017-09-08 17:46:32 +0800

    feat(sample) bring back old metisa.js
    - update README

--- src/browser.js
+++ src/browser.js
@@ -2,6 +2,8 @@ const MetisaDom = require('./Metisa/dom');
 const util = require('./util');
 
 module.exports = (function() {
-  if (util.environment !== 'browser' ) { return console.warn('Metisa browser can only run inside a browser'); }
+  if (util.environment !== 'browser' ) {
+    return console.warn('Metisa browser can only run inside a browser');
+  }
   window.Metisa = new MetisaDom();
 })();
