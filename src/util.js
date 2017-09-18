b32e3958de0337a3d985c40b682890fc1d8d411e
tszyanalau <amy@altitudelabs.com>
2017-09-18 18:44:29 +0800

    api doc for developers
    amend descriptions
    
    add private tag
    
    correct IFrame def

--- src/util.js
+++ src/util.js
@@ -1,15 +1,36 @@
-module.exports = (function() {
-  return {
-    environment: typeof window === 'object' ? 'browser' : 'node',
-    compose: function(original) {
-      return function() {
-        const compositions = Array.prototype.slice.call(arguments);
-        var composed = original;
-        for (var i = 0; i < compositions.length; i++) {
-          composed = compositions[i](composed);
-        }
-        return composed;
+/**
+ * Gets SDK utilities
+ *
+ * @private
+ * @returns {UtilObj}
+ */
+function getUtil() {
+  /**
+   * Object containing utililities
+   * @typedef {Object} UtilObj
+   * @property {string} environment Environment that Metisa object is exposed to. Returns `'browser'` or `node`.
+   * @property {function} compose [Function](#getutilcomposefuncoriginal-⇒-class) for composing classes
+   * @memberof getUtil
+   */
+  /**
+   * Returns a composed class.
+   *
+   * @param  {class} original Class to be composed to.
+   * @return {class}
+   */
+  var composeFunc = function(original) {
+    return function() {
+      const compositions = Array.prototype.slice.call(arguments);
+      var composed = original;
+      for (var i = 0; i < compositions.length; i++) {
+        composed = compositions[i](composed);
       }
+      return composed;
     }
   };
-})();
+  return {
+    environment: typeof window === 'object' ? 'browser' : 'node',
+    compose: composeFunc,
+  };
+}
+module.exports = (getUtil)();
